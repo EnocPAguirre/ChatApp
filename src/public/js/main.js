c89b5ebb2b638f36c8b2434689c7858377c71e39
Enoc <enocpinedaa@gmail.com>
2019-01-03 20:06:28 -0600

    Versión 2.0 del chatApp

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,6 +7,29 @@ const $messageForm = $('#message-form');
 const $mesageBox = $('#message');
 const $chat = $('#chat');
 
+//Obtener el dom desde el nickname form
+const $nickForm = $('#nickForm');
+const $nickname = $('#nickname');
+const $nickError = $('#nickError');
+
+const $users = $('#usernames');
+
+//Evento
+$nickForm.submit(e => {
+  e.preventDefault();
+  socket.emit('nuevo usuario', $nickname.val(), data => {
+    if (data) {
+      $('#nickWrap').hide();
+      $('#contenWrap').show();
+
+    } else {
+      $nickError.html('<div class="alert alert-danger"> Ese usuario ya existe</div>');
+    }
+    $nickname.val('');
+  });
+
+});
+
 //Eventos
 $messageForm.submit( e => {
   e.preventDefault();
@@ -16,6 +39,14 @@ $messageForm.submit( e => {
 
 //Escuchar el evento desde el servidor
 socket.on('Nuevo mensaje', function(data) {
-  $chat.append(data + '</br>')
+  $chat.append('<b>' +  data.nick + '</b>: ' + data.msg + '<br/>')
+});
+
+socket.on('usernames', data => {
+  let html = '';
+  for (let i = 0; i < data.length; i++) {
+    html += `<p><i class ="fas fa-user "></i>${data[i]}</p>`
+  }
+  $users.html(html);
 });
 })
